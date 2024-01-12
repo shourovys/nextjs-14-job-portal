426224d36ccbeedac7806905e00a5a9149b31d94
Shourov Saha <shourovys@gmail.com>
2024-01-12 18:17:17 +0600

    feat: fetch and render jobs in homepage server component

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,11 @@
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function Badge({ children }: IProps) {
+  return (
+    <span className="rounded border bg-muted px-2 py-0.5 text-sm font-medium text-muted-foreground">
+      {children}
+    </span>
+  );
+}
